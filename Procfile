271217f3d9a7b6e235a1ed7aaca62e079756e584
Wilker Vinicius <wilkervinicius@gmail.com>
2019-05-26 17:39:38 -0300

    Config App Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn racha.wsgi --log-file -
--- racha/settings.py
+++ racha/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5hqq#m03-xd66l+!202o7%7h=4(_i_64q%mt&7y+z==1i_akm5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['10.0.2.2','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['backend-racha.herokuapp.com','10.0.2.2','localhost','127.0.0.1']
 
 
 # Application definition
@@ -78,12 +80,10 @@ WSGI_APPLICATION = 'racha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+from dj_database_url import parse as dburl
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #AUTH_USER_MODEL = 'accounts.User'
 
